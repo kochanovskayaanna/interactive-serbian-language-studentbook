92d5c578cc3a6d09c78462f8a1156847c8c0016b
kochanovskayaanna <kochanovskayaanna@yandex.ru>
2023-01-27 03:22:51 +0300

    Dynamic alphabet table generation

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import render_template, Flask, request
+import pandas as pd
 app = Flask(__name__, template_folder=".")
+from jinja2 import Template
+
+letters = ["letter-a", "letter-b", "letter-v", "letter-g", "letter-d", "letter-dj", "letter-e", "letter-ž", "letter-z",
+    "letter-i", "letter-j", "letter-k", "letter-l", "letter-lj", "letter-m", "letter-n", "letter-nj", "letter-o",
+    "letter-p", "letter-r", "letter-s", "letter-t", "letter-ć", "letter-u", "letter-f", "letter-h",
+    "letter-c", "letter-č", "letter-dž", "letter-š"]
 
 @app.route('/')
 def main():
@@ -11,7 +18,9 @@ def useful():
 
 @app.route('/alphabet.html')
 def alphabet():
-    return render_template('alphabet.html')
+    alpha_csv = pd.read_csv('alphabet.csv', delimiter=';')
+    alpha_list = alpha_csv.to_dict(orient='records')
+    return render_template('alphabet.html', alpha_list=alpha_list, letters=letters)
 
 @app.route('/biti.html')
 def biti():
